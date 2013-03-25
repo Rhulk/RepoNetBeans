182e1d2941529a88e9124bb01cc345a2a88b37e9
Administrador <Administrador@192.168.153.128>
2013-03-25 16:29:47 +0100

    #6 -    Añadido el JLabel etiquta_Imagen que sera donde se muestre la imagen 	add new JPanel para el boton y el JTextField respuesta

--- src/resolutorcaptcha/CaptchaGUI.java
+++ src/resolutorcaptcha/CaptchaGUI.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package resolutorcaptcha;
 
 import java.awt.Color;
@@ -15,50 +11,58 @@ import javax.swing.JTextField;
 
 /**
  *
- * @author Administrador
+ * @author Enrique Carvajal 25/03/2013
  */
 public class CaptchaGUI extends JFrame {
-   Resolutor resolutor = new Resolutor(this);
+   Resolutor resolutor = new Resolutor(this); // vinculamos con la clase Resolutor
     // separacion top
     JPanel filaTop = new JPanel();
     // configurar fila 1
     JPanel fila1 = new JPanel();
-        // pendiente de saber donde mostraremos la imagen
-        JLabel imagen = new JLabel();
+    JLabel etiqueta_Imagen = new JLabel();//Etiqueta donde se mostrara la imagen
     // configurar fila 2
     JPanel fila2 = new JPanel();
+    JButton enviar = new JButton("enviar");    
+    JTextField respuesta = new JTextField(15);
+    // configurar fila 3
+    JPanel fila3 = new JPanel();
     JButton iniciar = new JButton("Iniciar");
     JButton parar = new JButton("Parar");
-    JTextField respuesta = new JTextField(15);
-    
-    // configurar fila 3
-        JPanel fila3 = new JPanel();
+    // configurar fila 4
+        JPanel fila4 = new JPanel();
     // constructor por defecto
     public CaptchaGUI(){
         super("Resolutor Captcha.");
-        setSize(750,270); // tamaño inicial de la ventana
+        setSize(750,370); // tamaño inicial de la ventana
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new GridLayout(4,1,10,10)); // mas simple
+        setLayout(new GridLayout(5,1,10,10)); // mas simple
         add(filaTop);
         // hasta aqui la confi del frame principal
         
         // añadimos los listener para los eventos de los botones
         iniciar.addActionListener(resolutor);
         parar.addActionListener(resolutor);
+        enviar.addActionListener(resolutor);
         
         // configuracion de los JPanel
         fila1.setLayout(new FlowLayout(FlowLayout.CENTER,10,10));
-        fila1.add(imagen);
+        fila1.add(etiqueta_Imagen);
         add(fila1);
         
         fila2.setLayout(new FlowLayout(FlowLayout.CENTER,20,20));
-        fila2.add(iniciar);
-        parar.setEnabled(false);
+        fila2.add(enviar);
+        enviar.setEnabled(false);
         fila2.add(respuesta);
-        fila2.add(parar);
         add(fila2);
         
+        fila3.setLayout(new FlowLayout(FlowLayout.CENTER,20,20));
+        fila3.add(iniciar);
+        fila3.add(parar);
+        parar.setEnabled(false);
         add(fila3);
+        
+        fila4.setLayout(new FlowLayout(FlowLayout.CENTER,20,20));
+        add(fila4);
         
         
         setVisible(true);
