2612408e41997c9eeab025de7281312733e04fbb
Administrador <Administrador@192.168.153.128>
2013-03-25 18:12:09 +0100

    #3-     Añadido el metodo esperarCaptcha: 		muesta la imagen esperardoCaptcha mientras se recibe el captcha. 	Modificado el metodo enviarRespuesta: 		deshabilitando el el boton enviar, reseteando el campo respuesta a " " y volviendo a llamar al metodo conectar para seguir a 			la escucha. 	Modificado el metodo obtenerMostrarImagen: 		Ahora ya muestra la imagen en el JLabel y ya no crea el archivo .jpg de la imagen. 	Modificado el metodo conectar: 		Ahora llama al metodo esperarCapcha para resetear la imagen aterior mientras llega la nueva.

--- src/resolutorcaptcha/Resolutor.java
+++ src/resolutorcaptcha/Resolutor.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package resolutorcaptcha;
 
 import java.awt.event.*;
@@ -9,6 +5,7 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -17,6 +14,7 @@ import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 /**
  *
  * @author Enrique Carvajal 25/03/2013
@@ -29,9 +27,8 @@ public class Resolutor  implements ItemListener, ActionListener, Runnable{
     private DataOutputStream dos;
     private DataInputStream dis;
     private ObjectInputStream entrada_imagen;
-    
-    
-    
+//    private JLabel etiqueta_Imagen_Original;//Etiqueta donde se mostrara la imagen
+
     public Resolutor(CaptchaGUI in){
         gui =in;        
     }
@@ -45,15 +42,17 @@ public class Resolutor  implements ItemListener, ActionListener, Runnable{
             dos.writeUTF("resolucion");
 //            System.out.println("esperando respuesta");
             System.out.println(dis.readUTF());  // El server nos devuelve el foco. Para recibir la imagen.
-//            obtenerMostrarImagen(); // leemos la imagen.
+            obtenerMostrarImagen(); // leemos la imagen.
             System.out.println("recibida");            
         } catch (UnknownHostException ex) {
             Logger.getLogger(Resolutor.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(Resolutor.class.getName()).log(Level.SEVERE, null, ex);
-        }          
+        }       
     }
-    void conectar(){
+    void conectar() throws IOException{
+        esperarCaptcha();
+        System.out.println("conectar");
         conectando = new Thread(this);
         gui.iniciar.setEnabled(false);
         gui.parar.setEnabled(true);
@@ -67,19 +66,28 @@ public class Resolutor  implements ItemListener, ActionListener, Runnable{
     }
     void enviarRespuesta() throws IOException{
         dos.writeUTF(gui.respuesta.getText());
+        gui.enviar.setEnabled(false);
+        gui.respuesta.setText("");
+        conectar(); // continuar a la espera de captchas
+        
     }
     void obtenerMostrarImagen() throws IOException{
+//        System.out.println("obtener imagen");
                try {
                 entrada_imagen = new ObjectInputStream(sk.getInputStream());
                 byte[] bytesImagen = (byte[]) entrada_imagen.readObject();
                 ByteArrayInputStream entradaImagen = new ByteArrayInputStream(bytesImagen);
                 BufferedImage bufferedImage = ImageIO.read(entradaImagen);
- 
-                String nombreFichero="C:\\Documents and Settings\\Administrador\\Escritorio\\CaptchaIn.jpg";
-                System.out.println("Generando el fichero: "+nombreFichero );
-                FileOutputStream out = new FileOutputStream(nombreFichero);
-                // esbribe la imagen a fichero
-                ImageIO.write(bufferedImage, "jpg", out);
+                // cargar la imagen en un JLabel.
+                ImageIcon icono=new ImageIcon(bufferedImage);
+                gui.etiqueta_Imagen.setIcon(icono);
+                gui.enviar.setEnabled(true);// para poder enviar la respuesta
+                // Guardar la imagen en fichero.
+//                String nombreFichero="C:\\Documents and Settings\\Administrador\\Escritorio\\CaptchaIn.jpg";
+//                System.out.println("Generando el fichero: "+nombreFichero );
+//                FileOutputStream out = new FileOutputStream(nombreFichero);
+//                // esbribe la imagen a fichero
+//                ImageIO.write(bufferedImage, "jpg", out);
             }
  
             // atrapar problemas que pueden ocurrir al tratar de leer del cliente
@@ -111,6 +119,11 @@ public class Resolutor  implements ItemListener, ActionListener, Runnable{
     @Override
     public void itemStateChanged(ItemEvent e) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    public void esperarCaptcha() throws IOException{
+        BufferedImage esperar= ImageIO.read(new File("C:\\Documents and Settings\\Administrador\\Mis documentos\\NetBeansProjects\\ResolutorCaptcha\\src\\resolutorcaptcha\\esperandoCaptcha.jpg"));
+        ImageIcon icono=new ImageIcon(esperar);
+        gui.etiqueta_Imagen.setIcon(icono);
     }
     
 }
